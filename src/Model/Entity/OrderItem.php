c3aecb09b5f46baa6c5f79f41a3187d3706048d5
Werner Burat <werner.burat@gmail.com>
2018-10-04 13:15:13 -0400

    Order items and products models

---
+++ src/Model/Entity/OrderItem.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * OrderItem Entity
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $product_id
+ * @property int $quantity
+ * @property \Cake\I18n\FrozenTime $created
+ * @property \Cake\I18n\FrozenTime $modified
+ *
+ * @property \App\Model\Entity\CustomerOrder $customer_order
+ * @property \App\Model\Entity\Product $product
+ */
+class OrderItem extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'order_id' => true,
+        'product_id' => true,
+        'quantity' => true,
+        'created' => true,
+        'modified' => true,
+        'customer_order' => true,
+        'product' => true
+    ];
+}
